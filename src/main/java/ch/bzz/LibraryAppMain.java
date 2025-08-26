577bd649901dffd1e52a4baa0c9f78e22e2f136b
Im23a-demkoa <demkoa@bzz.ch>
2025-08-26 10:28:07 +0200

    Scanner and class books made

--- src/main/java/ch/bzz/LibraryAppMain.java
+++ src/main/java/ch/bzz/LibraryAppMain.java
@@ -1,8 +1,36 @@
 package ch.bzz;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class LibraryAppMain {
 
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        String[] commands = {"help", "quit", "cool", "bye", "say"};
+        Scanner scanner = new Scanner(System.in); // nur einmal erstellen
+
+        while (true) {
+            String command = scanner.nextLine();
+
+            if (command.equals("quit")) {
+                break;
+            } else if (command.equals("help")) {
+                System.out.println(Arrays.toString(commands));
+            } else {
+                System.out.println("Eingabe nicht als Befehl erkannt" + command);
+                System.out.println("mochtest du hinzufugen Yes/No");
+                if (scanner.nextLine().equals("Yes")) {
+                    System.out.println("Type new command");
+                    String newString = scanner.nextLine();
+                    String[] newArray = Arrays.copyOf(commands, commands.length + 1);
+                    newArray[newArray.length - 1] = newString;
+                    commands = newArray;
+                } else {
+                    break;
+                }
+            }
+        }
     }
+
 }
---
+++ src/main/java/ch/bzz/books.java
@@ -0,0 +1,16 @@
+package ch.bzz;
+
+public class books {
+    int id;
+    String isbn;
+    String title;
+    String author;
+    int year;
+     public books(int id, String isbn, String title, String author, int year) {
+         this.id = id;
+         this.isbn = isbn;
+         this.title = title;
+         this.author = author;
+         this.year = year;
+     }
+}
